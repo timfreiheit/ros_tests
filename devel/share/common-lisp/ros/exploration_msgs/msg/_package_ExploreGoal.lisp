e46e83cdeef0a3091d22f4cd5be8f684d10d02ca
Tim Freiheit <timfreiheit@hotmail.de>
2015-10-18 17:17:07 +0200

    -

---
+++ devel/share/common-lisp/ros/exploration_msgs/msg/_package_ExploreGoal.lisp
@@ -0,0 +1,6 @@
+(cl:in-package exploration_msgs-msg)
+(cl:export '(EXPLORE_BOUNDARY-VAL
+          EXPLORE_BOUNDARY
+          EXPLORE_CENTER-VAL
+          EXPLORE_CENTER
+))
---
+++ devel/share/exploration_msgs/cmake/exploration_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(exploration_msgs_MSG_INCLUDE_DIRS "/home/tim/Dokumente/Git/ros_tests/devel/share/exploration_msgs/msg")
+set(exploration_msgs_MSG_DEPENDENCIES actionlib_msgs;geometry_msgs)
